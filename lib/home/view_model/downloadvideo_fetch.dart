7bded475902ff27d7b8f3d74c4e2a30ed8573739
junayedahamed <junayedahamed660@gmail.com>
2026-04-22 19:26:33 +0600

    Add video fetching functionality and integrate photo_manager plugin

--- lib/home/view_model/downloadvideo_fetch.dart
+++ lib/home/view_model/downloadvideo_fetch.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class DownloadvideoFetch extends ChangeNotifier {
-  List videoList = [];
+  List<AssetEntity> videoList = [];
+  bool isLoading = false;
+
   Future<void> downloadVideo() async {
+    isLoading = true;
     notifyListeners();
+
+    try {
+      // Request permission to access files
+      final PermissionState ps = await PhotoManager.requestPermissionExtend();
+      if (ps.isAuth) {
+        // Fetch all video albums
+        final List<AssetPathEntity> albums = await PhotoManager.getAssetPathList(
+          type: RequestType.video,
+        );
+
+        if (albums.isNotEmpty) {
+          // Fetch all videos from all albums or just the "Recent" one (usually the first)
+          final List<AssetEntity> allVideos = [];
+          for (var album in albums) {
+            final List<AssetEntity> videos = await album.getAssetListRange(
+              start: 0,
+              end: await album.assetCountAsync,
+            );
+            allVideos.addAll(videos);
+          }
+          videoList = allVideos;
+        }
+      } else {
+        // Handle permission denied
+        PhotoManager.openSetting();
+      }
+    } catch (e) {
+      debugPrint('Error fetching videos: $e');
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import photo_manager
 import video_player_avfoundation
 import volume_controller
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   VolumeControllerPlugin.register(with: registry.registrar(forPlugin: "VolumeControllerPlugin"))
 }
