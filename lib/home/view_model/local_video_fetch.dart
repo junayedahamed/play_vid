b7db1277202d4465833e1784724f17139d9c52f0
junayedahamed <junayedahamed660@gmail.com>
2026-04-22 19:33:56 +0600

    update

---
+++ lib/home/view_model/local_video_fetch.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+class LocalVideoFetch extends ChangeNotifier {
+  List<AssetEntity> videoList = [];
+  bool isLoading = false;
+
+  Future<void> downloadVideo() async {
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      // Request permission to access files
+      final PermissionState ps = await PhotoManager.requestPermissionExtend();
+      if (ps.isAuth) {
+        // Fetch all video albums
+        final List<AssetPathEntity> albums =
+            await PhotoManager.getAssetPathList(type: RequestType.video);
+
+        if (albums.isNotEmpty) {
+          // Fetch all videos from all albums or just the "Recent" one (usually the first)
+          final List<AssetEntity> allVideos = [];
+          for (var album in albums) {
+            final List<AssetEntity> videos = await album.getAssetListRange(
+              start: 0,
+              end: await album.assetCountAsync,
+            );
+            allVideos.addAll(videos);
+          }
+          videoList = allVideos;
+        }
+      } else {
+        // Handle permission denied
+        PhotoManager.openSetting();
+      }
+    } catch (e) {
+      debugPrint('Error fetching videos: $e');
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
